f152c30f2b3e5908277f4b8d1fc0e43b8f1c5a46
JoseLobos <88002545+JoseLobos@users.noreply.github.com>
2022-06-11 17:44:58 -0600

    video

--- app/src/main/java/App/download/quizizz_maker/Ayuda2.java
+++ app/src/main/java/App/download/quizizz_maker/Ayuda2.java
@@ -1,10 +1,12 @@
 package App.download.quizizz_maker;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,28 +14,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Ayuda2 extends AppCompatActivity {
-    Button Regresar;
-    VideoView videoView;
+    VideoView vv1;
+    TextView tv1,tv2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda2);
-        Button  Regresar = (Button)this.findViewById(R.id.btnRegresar);
-        Regresar.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent ayuda1 = new Intent(Ayuda2.this,Ayuda1.class);
-                startActivity(ayuda1);
-            }
-        });
-
-        videoView = (VideoView)findViewById(R.id.videoView);
-
-        String path = "android.resource://"+ getPackageName() + "/" +
-                R.raw.video;
-        videoView.setVideoURI(Uri.parse(path));
-        videoView.start();
+        vv1=findViewById(R.id.vv1);
+        tv1=findViewById(R.id.tv1);
+        tv2=findViewById(R.id.tv2);
 
     }
+    public void inicar (View view)
+    {
+        tv1.setText("ESTADO: DESCARGANDO DE INTERNET");
+        vv1.setVideoURI(Uri.parse("https://youtu.be/ltNCaZcPRgA"));
+        vv1.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                tv1.setText("ESTADO: REPRODUCIENDO");
+                tv2.setText("DURACION:"+(vv1.getDuration()/1000)+"seg.");
+                vv1.start();
+            }
+        });
+    }
+
+
 }
 
